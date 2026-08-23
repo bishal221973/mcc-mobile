c2f772730e3c0d4b09be007412ce30fcbf67c976
bishal221973 <bishalcodeslaravel@gmail.com>
2026-08-23 17:07:58 +0545

    Firebase setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,46 @@
 import React, { useEffect } from 'react';
-import { Alert, StyleSheet } from 'react-native';
+import { Alert, StyleSheet, PermissionsAndroid } from 'react-native';
 import StackNavigation from './src/navigation/StackNavigation';
 import Toast from 'react-native-toast-message';
 import AuthMonitor from './src/services/AuthMonitor';
-
+import {
+  getMessaging,
+  registerDeviceForRemoteMessages,
+  getToken,
+} from '@react-native-firebase/messaging';
 const App = () => {
+
+  const requestPermission = async () => {
+    try {
+      const res = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
+      if (res == PermissionsAndroid.RESULTS.GRANTED) {
+        // Reques for tokkken
+        requestToken();
+      } else {
+        Alert.alert("Permission Denied");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const requestToken = async () => {
+    try {
+      const messaging = getMessaging();
+      // Optional: not needed in most cases, and a no-op if already registered.
+      await registerDeviceForRemoteMessages(messaging);
+      const token = await getToken(messaging);
+
+      console.log(token)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
 
   useEffect(() => {
     // Alert.alert("ss",'ada')
